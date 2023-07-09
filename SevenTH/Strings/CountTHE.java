9fec2f4c96dad73a7e3caea9361462c522fea5d2
Nova <nileshtelang07n5@gmail.com>
2023-07-10 00:19:47 +0530

    "Count The Words + RevWords In String"

---
+++ SevenTH/Strings/CountTHE.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class CountTHE {
+    static int wordcount(String string) {
+        int count = 0;
+
+        char ch[] = new char[string.length()];
+        for (int i = 0; i < string.length(); i++) {
+            ch[i] = string.charAt(i);
+            if (((i > 0) && (ch[i] != ' ') && (ch[i - 1] == ' ')) || ((ch[0] != ' ') && (i == 0)))
+                count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter The String : ");
+        String s = sc.nextLine();
+
+        System.out.println("Word Count :  "+ wordcount(s));
+    }
+}
--- SevenTH/Strings/RevWords.java
+++ SevenTH/Strings/RevWords.java
@@ -1,15 +1,26 @@
 public class RevWords {
-    
     public static void main(String[] args) {
         String s = "Computer Is Fun";
-        String rev="" ;
-        for(int i=s.length()-1;i>=0;i--){
-            if(s.charAt(i)== ' '){
-                rev = rev +  s.substring(i, s.length());   
-            }else if(i==0){
-                rev = rev +  s.substring(i,s.length());
+        String rev = "";
+        
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                rev = rev + s.charAt(i);
+            } else {
+                System.out.print(reverseWord(rev) + " ");
+                rev = "";
             }
         }
-        System.out.println(rev);
+        
+        // Print the last word
+        System.out.print(reverseWord(rev));
+    }
+    
+    private static String reverseWord(String word) {
+        String reversedWord = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversedWord += word.charAt(i);
+        }
+        return reversedWord;
     }
 }
